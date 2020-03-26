ca4aec1c0c211b27d40279493ba8354e886fd3ac
rainofflower <rainofflower.yh@qq.com>
2020-03-26 11:29:50 +0800

    双重校验加锁单例模式加上volatile

--- src/main/java/Singleton/Singleton1.java
+++ src/main/java/Singleton/Singleton1.java
@@ -2,15 +2,12 @@ package Singleton;
 
 public class Singleton1 {
 	
-	private static Singleton1 uniqueInstance = null;
+	private static volatile Singleton1 uniqueInstance = null;
 	
-	private Singleton1(){
-		
-	};
+	private Singleton1(){}
 	
 	public static Singleton1 getInstance(){
-		if(uniqueInstance == null){		
-			
+		if(uniqueInstance == null){
 			synchronized (Singleton1.class) {				
 				if(uniqueInstance == null){
 					uniqueInstance = new Singleton1();
